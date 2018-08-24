22df90aa43b0dea1c417b506a4b6a1d73ed9df83
Asier Sánchez Elizazu <sancheza5@justizia.local>
2018-08-24 14:53:32 +0200

    Añadir graficos

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const API_KEY='4447bb19b3723dd3b884010caecfd3bb';
-const ROOT_URL =`http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},es`; 
     const request = axios.get(url);
 
-    console.log('Request: ', request);
+   
     return {
         type: FETCH_WEATHER,
         payload: request
